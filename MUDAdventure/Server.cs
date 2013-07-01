2ba1bae4e6deb93ba1d53b6804025c7c5169a4b0
Ryan Woodring <rdwoodring@gmail.com>
2013-06-30 20:34:29 -0400

    add functionality for reading rooms from an xml file

--- MUDAdventure/Server.cs
+++ MUDAdventure/Server.cs
@@ -7,6 +7,9 @@ using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 using System.Timers;
+using System.Xml;
+using System.IO;
+using System.Diagnostics;
 
 namespace MUDAdventure
 {
@@ -39,9 +42,10 @@ namespace MUDAdventure
             this.time = 0;
             this.hour = 0; 
 
-            //TODO: add code for loading rooms from DB
+            //TODO: add code for loading rooms from DB or XML file
             //rooms.Add("123", new Room()); etc., etc.
             //for now let's just create some rooms manually for testing purposes.
+            /*
             Room room = new Room("A Starting Place", "This is the starting room.  It is completely empty.", true, false, true, false, 0, 0, 0, true);
             this.rooms.Add(room.X.ToString() + "," + room.Y.ToString() + "," + room.Z.ToString(), room);
 
@@ -53,10 +57,111 @@ namespace MUDAdventure
 
             room = new Room("East of A Starting Place", "Nothing here.  Move along, move along.", true, false, false, true, 1, 0, 0, false);
             this.rooms.Add(room.X.ToString() + "," + room.Y.ToString() + "," + room.Z.ToString(), room);
+             * */
+            Console.WriteLine("Attempting to read Rooms.xml...");
+            this.ReadRoomFile();
+            if (rooms.Count > 0)
+            {
+                Console.WriteLine("Success!\r\nRead {0} rooms from the file.", rooms.Count);
+            }
 
             //TODO: add code for loading npcs from DB
             NPC npc = new NPC(0, 0, 0, "An NPC", "An NPC is standing here.  It has no form and nothing on.", new List<string> {"NPC"}, 60000, 10, 50, this.worldTimer, this.players, this.rooms);
             this.npcs.Add( npc);                                   
+        }
+
+        private void ReadRoomFile()
+        {
+            //TODO: replace this path with the root path eventually.  For now, it is always looking for this
+            //file in the debug folder, so we'll specify elsewhere.
+            if (File.Exists(@"..\..\Rooms.xml"))
+            {
+                /*using (StreamReader sr = new StreamReader(@"..\..\Rooms.xml"))
+                {
+                    //string contents = sr.ReadToEnd();
+                    //return contents;
+                }*/
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load(@"..\..\Rooms.xml");
+
+                XmlNodeList xmlrooms = doc.GetElementsByTagName("room");
+                for (int i = 0; i < xmlrooms.Count; i++)
+                {
+                    string name, desc;
+                    bool nexit, sexit, wexit, eexit, lighted;
+                    int x, y, z;
+
+                    //Debug.Print(xmlrooms[i].HasChildNodes.ToString());
+                    //Debug.Print(xmlrooms[i].ChildNodes.Count.ToString());
+                    //Debug.Print(xmlrooms[i].FirstChild.InnerText);
+                    name = xmlrooms[i].ChildNodes[0].InnerText;
+                    desc = xmlrooms[i].ChildNodes[1].InnerText;
+                    nexit = bool.Parse(xmlrooms[i].ChildNodes[2].InnerText);
+                    sexit = bool.Parse(xmlrooms[i].ChildNodes[3].InnerText);
+                    eexit = bool.Parse(xmlrooms[i].ChildNodes[4].InnerText);
+                    wexit = bool.Parse(xmlrooms[i].ChildNodes[5].InnerText);
+                    x = int.Parse(xmlrooms[i].ChildNodes[6].InnerText);
+                    y = int.Parse(xmlrooms[i].ChildNodes[7].InnerText);
+                    z = int.Parse(xmlrooms[i].ChildNodes[8].InnerText);
+                    lighted = bool.Parse(xmlrooms[i].ChildNodes[9].InnerText);
+
+                    Room room = new Room(name, desc, nexit, sexit, eexit, wexit, x, y, z, lighted);
+                    rooms.Add(x + "," + y + "," + z, room);
+                }                
+            }
+            else
+            {
+                Console.WriteLine("Couldn't locate the Rooms.xml file. Make sure that it is named corrected and in the correct directory.");
+                //return null;
+            }
+        }
+
+        private void ParseRoomsXml(string roomsXml)
+        {
+            using (XmlReader reader = XmlReader.Create(new StringReader(roomsXml)))
+            {
+                
+                while (!reader.EOF)
+                {
+                    string name, desc;
+                    bool nexit, sexit, wexit, eexit, lighted;
+                    int x, y, z;
+                
+                    reader.ReadToFollowing("name");
+                    name = reader.ReadElementContentAsString();
+
+                    reader.ReadToFollowing("description");
+                    desc = reader.ReadElementContentAsString();
+
+                    reader.ReadToFollowing("nexit");
+                    nexit = reader.ReadElementContentAsBoolean();
+
+                    reader.ReadToFollowing("sexit");
+                    sexit = reader.ReadElementContentAsBoolean();
+
+                    reader.ReadToFollowing("eexit");
+                    eexit = reader.ReadElementContentAsBoolean();
+
+                    reader.ReadToFollowing("wexit");
+                    wexit = reader.ReadElementContentAsBoolean();
+                    
+                    reader.ReadToFollowing("x");
+                    x = reader.ReadElementContentAsInt();
+
+                    reader.ReadToFollowing("y");
+                    y = reader.ReadElementContentAsInt();
+
+                    reader.ReadToFollowing("z");
+                    z = reader.ReadElementContentAsInt();
+                    
+                    reader.ReadToFollowing("lighted");
+                    lighted = reader.ReadElementContentAsBoolean();
+
+                    Room room = new Room(name, desc, nexit, sexit, eexit, wexit, x, y, z, lighted);
+                    this.rooms.Add(x + "," + y + "," + z, room);
+                }
+            }
         }
 
         private void ListenForClients()
