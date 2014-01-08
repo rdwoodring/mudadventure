fa08d738fca517e8a0df892066604991d32aa618
Ryan Woodring <rdwoodring@gmail.com>
2014-01-07 21:41:11 -0500

    start creating groundwork for 'flexible' skill system that can be easily modified by end-MUD creator with minimal/no coding

--- MUDAdventure/Skills/Skill.cs
+++ MUDAdventure/Skills/Skill.cs
@@ -7,11 +7,13 @@ namespace MUDAdventure.Skills
 {
     class Skill
     {
-        private string skillName;
-        private int difficulty, proficiency;
-        private int effectOnHP, effectOnMP, effectOnMoves, effectOnCarryWeight; //negative number for a "cost," positive number for a "bonus"
-        private int daggerProfRequired, swordProfRequired, axeProfRequired, parryProfRequired, dodgeProfRequired, roamerProfRequired, travellerProfRequired, packmuleProfRequired, oxenProfRequired, fieldmedicineProfRequired, sneakProfRequired, hideProfRequired, ambushProfRequired, pickpocketProfRequired, picklockProfRequired;
-        private int levelRequired;
+        protected string skillName;
+        protected int difficulty, proficiency;
+        protected int effectOnHP, effectOnMP, effectOnMoves, effectOnCarryWeight; //negative number for a "cost," positive number for a "bonus"
+        //protected int daggerProfRequired, swordProfRequired, axeProfRequired, parryProfRequired, dodgeProfRequired, roamerProfRequired, travellerProfRequired, packmuleProfRequired, oxenProfRequired, fieldmedicineProfRequired, sneakProfRequired, hideProfRequired, ambushProfRequired, pickpocketProfRequired, picklockProfRequired;
+        protected Dictionary<string, int> preliminaryPrerequisites;
+        protected Dictionary<Skill, int> finalPrerequisites;
+        protected int levelRequired;
 
         #region Constructors
 
@@ -21,7 +23,7 @@ namespace MUDAdventure.Skills
         public Skill() { }
 
         /// <summary>
-        /// Constructor with arguments
+        /// Constructor with arguments that specify the skill's effects.  This constructor does not require a list of preliminary prerequisite skills (i.e. skills that must be learned to a certain percentage before this skill can be learned), so this constructor is useful for instantiating 'base' skills that are available to low level characters.
         /// </summary>
         /// <param name="name">The skill's name, i.e. parry, dodge, etc.</param>
         /// <param name="diff">How difficult the skill is to learn.  1 being easiest, 10 being hardest.</param>
@@ -41,11 +43,11 @@ namespace MUDAdventure.Skills
             this.effectOnCarryWeight = carryeffect;
 
             this.levelRequired = 0;
-            this.daggerProfRequired = this.swordProfRequired = this.axeProfRequired = this.parryProfRequired = this.dodgeProfRequired = this.roamerProfRequired = this.travellerProfRequired = this.packmuleProfRequired = this.oxenProfRequired = this.fieldmedicineProfRequired = this.sneakProfRequired = this.hideProfRequired = this.ambushProfRequired = this.pickpocketProfRequired = this.picklockProfRequired = 0;
+            this.preliminaryPrerequisites = null;
         }
 
         /// <summary>
-        /// Constructor with arguments
+        /// Constructor with arguments specify the skill's effects.  This constructor accepts a dictionary of prerequisite skills in a key/pair format.  The key being the skill's name and the value being the required proficiency in that skill in order to learn this skill.
         /// </summary>
         /// <param name="name">The skill's name, i.e. parry, dodge, etc.</param>
         /// <param name="diff">How difficult the skill is to learn.  1 being easiest, 10 being hardest.</param>
@@ -55,22 +57,8 @@ namespace MUDAdventure.Skills
         /// <param name="mpeffect">The effect that using this skill has on Moves.  A positive number is an Moves bonus, a negative number is an Moves cost.</param>
         /// <param name="carryeffect">The effect that using this skill has on Carrying capacity. A positive number is a Carrying capacity bonus, a negative number is a Carrying capacity cost</param>
         /// <param name="levelreq">The required minimum level needed to begin practicing this skill. 0-100</param>
-        /// <param name="dagreq">The required proficiency in the dagger skill to begin practicing this skill. 0-100</param>
-        /// <param name="swordreq">The required proficiency in the sword skill to begin practicing this skill. 0-100</param>
-        /// <param name="axereq">The required proficiency in the axe skill to begin practicing this skill. 0-100</param>
-        /// <param name="parryreq">The required proficiency in parry skill to begin practicing this skill. 0-100</param>
-        /// <param name="dodgereq">The required proficiency in dodge skill to begin practicing this skill. 0-100</param>
-        /// <param name="roamerreq">The required proficiency in the roamer skill to begin practicing this skill. 0-100</param>
-        /// <param name="travellerreq">The required proficiency in the traveller skill to begin practicing this skill. 0-100</param>
-        /// <param name="packmulereq">The required proficiency in the packmule skill to begin practicing this skill. 0-100</param>
-        /// <param name="oxenreq">The required proficiency in the oxen skill to begin practicing this skill. 0-100</param>
-        /// <param name="fieldmedreq">The required proficiency in the field medicine skill to begin practicing this skill. 0-100</param>
-        /// <param name="sneakreq">The required proficiency in the sneak skill to begin practicing this skill. 0-100</param>
-        /// <param name="hidereq">The required proficiency in the hide skill to begin practicing this skill. 0-100</param>
-        /// <param name="ambushreq">The required proficiency in the ambush skill to begin practicing this skill. 0-100</param>
-        /// <param name="pickpocketreq">The required proficiency in the pickpocket skill to begin practicing this skill. 0-100</param>
-        /// <param name="picklockreq">The required proficiency in the picklock skill to begin practicing this skill. 0-100</param>
-        public Skill(string name, int diff, int prof, int hpeffect, int mpeffect, int moveseffect, int carryeffect, int levelreq, int dagreq, int swordreq, int axereq, int parryreq, int dodgereq, int roamerreq, int travellerreq, int packmulereq, int oxenreq, int fieldmedreq, int sneakreq, int hidereq, int ambushreq, int pickpocketreq, int picklockreq)
+        /// <param name="_preliminaryPrerequisites">A dictionary of "prerequisite" skills that must be learned before learning this skill.  Passed as key/pair format where the key is the skill's name as a string and the value is the required proficiency.  Profiency must be an int between 1-100.</param>
+        public Skill(string name, int diff, int prof, int hpeffect, int mpeffect, int moveseffect, int carryeffect, int levelreq, Dictionary<string, int> _preliminaryPrerequisites)
         {
             this.skillName = name;
             this.difficulty = diff;
@@ -83,24 +71,39 @@ namespace MUDAdventure.Skills
 
             this.levelRequired = levelreq;
 
-            this.daggerProfRequired = dagreq;
-            this.swordProfRequired = swordreq;
-            this.axeProfRequired = axereq;
-            this.parryProfRequired = parryreq;
-            this.dodgeProfRequired = dodgereq;
-            this.roamerProfRequired = roamerreq;
-            this.travellerProfRequired = travellerreq;
-            this.packmuleProfRequired = packmulereq;
-            this.oxenProfRequired = oxenreq;
-            this.fieldmedicineProfRequired = fieldmedreq;
-            this.sneakProfRequired = sneakreq;
-            this.hideProfRequired = hidereq;
-            this.ambushProfRequired = ambushreq;
-            this.pickpocketProfRequired = pickpocketreq;
-            this.picklockProfRequired = picklockreq;
+            this.preliminaryPrerequisites = _preliminaryPrerequisites;
         }
 
         #endregion
+
+        public void InitializeSkill(SkillTree skillTree)
+        {
+            if (this.preliminaryPrerequisites.Count > 0)
+            {
+                foreach(KeyValuePair<string, int> preReq in this.preliminaryPrerequisites)
+                {
+                    if (preReq.Value <= 0 || preReq.Value > 100)
+                    {
+                        //TODO: throw an exception.  Can't have a prerequisite that is less than 0% proficiency or greater than 100%
+                    }
+                    else
+                    {
+                        Skill querySkill = (from skill in skillTree.Skills
+                                            where skill.SkillName.ToLower() == preReq.Key.ToLower()
+                                            select skill).FirstOrDefault();
+
+                        if (querySkill != null)
+                        {
+                            this.finalPrerequisites.Add(querySkill, preReq.Value);
+                        }
+                        else
+                        {
+                            //TODO: throw an exception.  Couldn't find the skill defined in the skill tree.  User should go back and review their skill tree file to ensure that all "prerequisite" skills that are referenced are defined.
+                        }
+                    }
+                }
+            }
+        }
 
         #region Attribute Accessors
 
@@ -119,6 +122,12 @@ namespace MUDAdventure.Skills
         {
             get { return this.proficiency; }
             set { this.proficiency = value; }
+        }
+
+        public int LevelRequired
+        {
+            get;
+            set;
         }
 
         #endregion
---
+++ MUDAdventure/Skills/SkillTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MUDAdventure.Skills
+{
+    class SkillTree
+    {
+        private List<Skill> skills;
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public SkillTree() { }
+
+        /// <summary>
+        /// Constructor with arguments
+        /// </summary>
+        /// <param name="_skills">A list of all skills</param>
+        public SkillTree(List<Skill> _skills)
+        {
+            this.skills = _skills;
+        }
+
+        #endregion
+
+        public void InitializeSkillTree()
+        {
+            if (this.skills != null)
+            {
+                foreach(Skill s in this.skills)
+                {
+                    s.InitializeSkill(this);
+                }
+            }
+            else
+            {
+                //TODO: throw an exception or alert the user, then abort the program.  We can't initialize an empty skill tree.
+            }
+        }
+
+        #region Attribute Accessors
+
+        public List<Skill> Skills
+        {
+            get { return this.skills; }
+            set { this.skills = value; }
+        }
+
+        #endregion
+    }
+}
